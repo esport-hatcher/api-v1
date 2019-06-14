833e5751343450783af97f16394c111bfd67cdef
Arthur <arthur.gamblin.dev@gmail.com>
2019-06-14 13:48:24 +0200

    fix testing for travis

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,19 +1,18 @@
 import db from '@db';
-import { initUser } from '../db/models/User';
+import { initUser } from '@models/User';
 
 jest.setTimeout(30000);
 const OLD_ENV = process.env;
 
 beforeAll(async () => {
-    initUser(true);
     process.env = { ...OLD_ENV };
     if (process.env.NODE_ENV !== 'CI') {
         process.env.NODE_ENV = 'test';
     }
-    await db.init(true, true);
+    initUser(process.env.NODE_ENV === 'test');
+    return db.init(true, process.env.NODE_ENV === 'test');
 });
 
 afterAll(async () => {
-    process.env.NODE_ENV = 'dev';
-    await db.close(true);
+    return db.close(process.env.NODE_ENV === 'test');
 });
