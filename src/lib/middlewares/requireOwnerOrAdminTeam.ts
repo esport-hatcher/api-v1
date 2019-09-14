229265f88d8e0372e1be07c8fc83c45bd541ab90
Yuu <yunhan.xia@epitech.eu>
2019-09-14 18:27:33 +0900

    refacto middleware to adapt CI

--- src/lib/middlewares/requireOwnerOrAdminTeam.ts
+++ src/lib/middlewares/requireOwnerOrAdminTeam.ts
@@ -9,22 +9,18 @@ export const requireOwnerOrAdminTeam = async (
     _res: Response,
     next: NextFunction
 ) => {
-    try {
-        const { user } = req;
-        const { teamId } = req.params;
+    const { user } = req;
+    const { teamId } = req.params;
 
-        const team = await Team.findByPk(teamId);
-        const users = await team.getUsers();
-        const teamUser = users.find(teamUser => teamUser.id === user.id);
-        if (
-            !teamUser ||
-            (teamUser.TeamUser.role !== 'Owner' &&
-                teamUser.TeamUser.role !== 'Admin')
-        ) {
-            return next(unauthorizedError());
-        }
-        return next();
-    } catch (err) {
-        return next(err);
+    const team = await Team.findByPk(teamId);
+    const users = await team.getUsers();
+    const teamUser = users.find(teamUser => teamUser.id === user.id);
+    if (
+        !teamUser ||
+        (teamUser.TeamUser.role !== 'Owner' &&
+            teamUser.TeamUser.role !== 'Admin')
+    ) {
+        return next(unauthorizedError());
     }
+    return next();
 };
