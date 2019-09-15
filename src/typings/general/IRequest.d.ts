54e107501c2961b6e01ce3299df2706a8b59dd48
Arthur <arthur.gamblin@epitech.eu>
2019-09-15 17:56:00 +0200

    added some commentary to IRequest

--- src/typings/general/IRequest.d.ts
+++ src/typings/general/IRequest.d.ts
@@ -2,8 +2,17 @@ import { Request } from 'express';
 import { User, Team } from '@models';
 
 export interface IRequest extends Request {
+    /**
+     * if the request is protected with requireAuth, it will store the user making the request
+     */
     owner: User;
+    /**
+     * If their is a :teamId in the route making the request, the team will be stored in this variable
+     */
     team: Team;
+    /**
+     * If their is a :userId in the route making the request, the user will be stored in this variable
+     */
     user: User;
     // tslint:disable-next-line: no-any
     body: { [key: string]: any | undefined };
