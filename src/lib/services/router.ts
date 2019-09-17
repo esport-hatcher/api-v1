278749fad6dc151e98ad9b6276d94f57a91e81ce
Arthur <arthur.gamblin@epitech.eu>
2019-09-17 18:03:36 +0200

    fix(eventsRoutes): fixed teamResolver
    Co-authored-by: Yunhan Xia <jimmyxia28@gmail.com>

--- src/lib/services/router.ts
+++ src/lib/services/router.ts
@@ -1,9 +1,4 @@
 import { Router } from 'express';
-import { userResolver, teamResolver, eventResolver } from '@routes';
 
 // tslint:disable-next-line: variable-name
-export const BaseRouter = (): Router =>
-    Router({ mergeParams: true })
-        .param('userId', userResolver)
-        .param('teamId', teamResolver)
-        .param('eventId', eventResolver);
+export const BaseRouter = (): Router => Router({ mergeParams: true });
