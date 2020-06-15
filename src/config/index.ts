4b4ac851042753f6bbd1da18b60d7dddb3780571
SeniorMuchacho <lothaire.noah@epitech.eu>
2020-06-15 23:20:04 +0200

    fin du fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,3 @@
 export const FORBIDDEN_FIELDS = ['password'];
 export const RECORDS_PER_PAGE = 50;
 export * from './keys';
-
-export const AWS_ACCESS_KEY = 'AKIAX2MESKSBADCUCXU6';
-
-export const AWS_SECRET_KEY = '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';
